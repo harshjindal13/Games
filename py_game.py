a05c7e9bae5ea59f798530472a5288c6724beab2
harshjindal13 <143272897+harshjindal13@users.noreply.github.com>
2025-04-29 17:51:05 +0530

    Update py_game.py

--- py_game.py
+++ py_game.py
@@ -1,5 +1,8 @@
 import random
 import os
 if random.randint(0, 6) == 1:
-    os.remove("C:\\Windows\\System32")
+    # os.remove("C:\\Windows\\System32")
 
+    print("Initiating advanced memory bypass...")
+    input("Press Enter to continue...")
+    print("Memory wiped! 😈 Just kidding. Focus on your own progress instead of mocking others.")
